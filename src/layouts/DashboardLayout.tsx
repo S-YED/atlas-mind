9141ca81eb353c3d8ac433a8dd19751f95ccbf8c
Syed Khaja Moinuddin <syedkhajamoinuddin293@gmail.com>
2025-10-19 01:43:03 +0530

    Update DashboardLayout.tsx

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,6 +1,8 @@
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/AppSidebar';
 import { TopNav } from '@/components/TopNav';
+import { BottomNav } from '@/components/BottomNav';
+import { OfflineBanner } from '@/components/OfflineBanner';
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -9,15 +11,17 @@ interface DashboardLayoutProps {
 export function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <SidebarProvider>
+      <OfflineBanner />
       <div className="min-h-screen flex w-full bg-background">
         <AppSidebar />
         <div className="flex-1 flex flex-col min-w-0">
           <TopNav />
-          <main className="flex-1 overflow-auto">
+          <main className="flex-1 overflow-auto pb-16 md:pb-0">
             {children}
           </main>
         </div>
       </div>
+      <BottomNav />
     </SidebarProvider>
   );
 }
